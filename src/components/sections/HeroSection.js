06e790ececa4f2e0e03a2406b16c4e8e1d9e5762
Manuel Ernesto <alemdadox@gmail.com>
2021-12-04 08:04:15 +0100

    setup the project

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {Link} from "gatsby";
+import styled from "styled-components";
+
+function HeroSection() {
+    return (
+        <Wrapper>
+            <ContentWrapper>
+                <TextWrapper>
+                    <Title>Design <br/> and code React apps</Title>
+                    <Description>Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete
+                        courses
+                        about the best tools.</Description>
+                </TextWrapper>
+            </ContentWrapper>
+        </Wrapper>
+    );
+}
+
+export default HeroSection;
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`;
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`;
+
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
+import HeroSection from "../components/sections/HeroSection";
 
 function IndexPage() {
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <SEO title="Home"/>
+            <HeroSection/>
+        </Layout>
+    )
 }
 
 export default IndexPage
