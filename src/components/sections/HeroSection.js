edd20bf73c85694e826f1436e76b4868940bfa9b
Manuel Ernesto <alemdadox@gmail.com>
2021-12-18 15:49:02 +0100

    add animations

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 import {H1, MediumText} from "../styles/TextStyles";
 import {themes} from "../styles/ColorsStyles";
 import PurchaseButton from "../buttons/PurchaseButton";
@@ -29,6 +29,19 @@ function HeroSection() {
 
 export default HeroSection;
 
+const animation = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+    filter: blur(10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+    filter: blur(0);
+  }
+`
+
 const Wrapper = styled.div`
   //background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
   overflow: hidden;
@@ -40,17 +53,34 @@ const ContentWrapper = styled.div`
   padding: 200px 30px;
   display: grid;
   grid-template-columns: 360px auto;
-`;
+`
 
 const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
-`;
+
+  > * {
+    opacity: 0;
+    animation: ${animation} 1s forwards;
+
+    :nth-child(1) {
+      animation-delay: 0s;
+    }
+
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+
+    :nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
+`
 
 const Title = styled(H1)`
   color: ${themes.dark.text1};
-`;
+`
 
 const Description = styled(MediumText)``
 
